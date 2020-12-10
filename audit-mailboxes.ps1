a3ae3f922c7c9422a62a1cd9020446d23439ad65
Ryan Chapman <rchapman@cylance.com>
2020-12-09 20:24:21 -0700

    Fix for MFANumber

--- audit-mailboxes.ps1
+++ audit-mailboxes.ps1
@@ -2,7 +2,7 @@ try {Get-MsolDomain -ErrorAction stop} catch {Write-Error -Category Authenticati
 try {Get-Mailbox -resultsize 1 -ErrorAction stop} catch {Write-Error -Category AuthenticationError  -Message "Exchange Online Not Connected" -RecommendedAction "Check readme file for instructions" -TargetObject "Office365 Exchange"  -CategoryTargetName "NotConnected" -CategoryTargetType "Online Services";exit} # Determine if Exchange sessions is connected
 
 $mailboxes = get-mailbox -resultsize unlimited -Filter {RecipientTypeDetails -ne "DiscoveryMailbox"} #Get all mailboxes except discovery mailboxes
-$mailboxes|Add-Member -MemberType NoteProperty -Name "MFAEnabled" -value "" # Add extra properties to mailbox object
+$mailboxes|Add-Member -MemberType NoteProperty -Name "MFANumber" -value "" # Add extra properties to mailbox object
 $mailboxes|Add-Member -MemberType NoteProperty -Name "DelegatedAccess" -value ""
 $mailboxes|Add-Member -MemberType NoteProperty -Name "Office365Administrator" -value ""
 $admins = Get-MsolRoleMember -RoleObjectId (Get-MsolRole -RoleName "Company Administrator").ObjectId
@@ -24,4 +24,4 @@ $mb = $mailboxes| ForEach-Object { # For each mailbox
     }
     return $_
 }
-$mb|select-object UserPrincipalName, MFAEnabled, DelegatedAccess, auditenabled, Office365Administrator|export-excel -IncludePivotTable -PivotRows AuditEnabled -PivotData @{UserPrincipalName = "count"} -IncludePivotChart -ChartType Doughnut -Show -Path ".\out.xlsx"
+$mb|select-object UserPrincipalName, MFANumber, DelegatedAccess, auditenabled, Office365Administrator|export-excel -IncludePivotTable -PivotRows AuditEnabled -PivotData @{UserPrincipalName = "count"} -IncludePivotChart -ChartType Doughnut -Show -Path ".\out.xlsx"
